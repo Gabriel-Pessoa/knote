a793e98685c5d8a08dc08b5f4116e0b6171237ec
Gabriel Pessoa <gbritopessoa@gmail.com>
2020-04-20 20:52:21 -0300

    works: last review

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const path = require('path')
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
 const multer = require('multer')
+const marked = require('marked')
 
 const app = express()
 
-const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/dev'
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/dev' 
 
 const port = process.env.PORT || 3000 // use a porta 3000, a menos que exista uma porta pré-configurada
+
+
 
 
 async function start() {
@@ -17,21 +20,33 @@ async function start() {
     app.set('views', path.join(__dirname, 'views')) //path.join() => resultado => c:\projeto\views
     app.use(express.static(path.join(__dirname, 'public'))) // para entregar arquivos estáticos (imagens, arquivos CSS, e arquivos JavaScript) em um diretório chamado public (c:\projeto\public)
 
-    app.get('/', (req, res) => {
+    app.get('/', async (req, res) => {
         res.render('index', { notes: await retrieveNotes(db) }) // recupera dados do banco de dados e carrega
     })
-
-    app.post('/note', multer().none(), async (req,res) => { // envio de formulário
+    
+    // envio de formulário
+    app.post(
+      '/note',
+     multer({ dest: path.join(__dirname, 'public/uploads/') }).single('image'),
+      async (req,res) => { 
         if(!req.body.upload && req.body.description) {
             await saveNote(db, { description: req.body.description })
             res.redirect('/')
+        } else if (req.body.upload && req.file) {
+            const link = `/uploads/${encodeURIComponent(req.file.filename)}`
+            res.render('index', {
+                content: `${req.body.description} ![](${link})`,
+                notes: await retrieveNotes(db)
+            })
         }
-    })
+      }
+    )
 
     app.listen(port, () => {
         console.log(`App rodando em http://localhost:${port}`)
     })
 }
+
 
 //Essa função continua tentando se conectar a um banco de dados MongoDB no URL especificado até que a conexão seja bem-sucedida.
 async function initMongo() { // Retorna um objeto async function
@@ -56,9 +71,15 @@ async function initMongo() { // Retorna um objeto async function
 
 
 async function retrieveNotes(db) {
-    const notes = (await db.find().toArray()).reverse()
-    return notes
+    const notes = (await db.find().toArray()).reverse() // Atribui a variável notes = um array revertido de um objetos com todas as ocorrências DB
+    return notes.map( it => {
+        return { ...it, description: marked(it.description)} //converte todas as notas em HTML antes de retornar
+    })
 }
 
+// função que salva uma única nota no banco de dados:
+async function saveNote(db, note) {
+    await db.insertOne(note)
+    }
 
 start()
